ad76376ea9da96c4834c79869289b9d8caafc70f
khaitri1912 <85475641+khaitri1912@users.noreply.github.com>
2025-06-03 23:57:35 +0700

    Added the Netcode and Start new game

---
+++ Assets/Scripts/Manager/PlayerUIManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerUIManager : MonoBehaviour
+{
+    public static PlayerUIManager instance;
+
+    [Header("NETWORK JOIN")]
+    [SerializeField] bool startGameAsClient;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startGameAsClient)
+        {
+            startGameAsClient = false;
+            // Must shutdown, because the game started as a host during the title screen
+            NetworkManager.Singleton.Shutdown();
+            // Then restart, as a Client
+            NetworkManager.Singleton.StartClient();
+        }
+    }
+}
